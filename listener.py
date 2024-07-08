4b22a5a8de401414ce67fa6578008db02ff40ac6
Great Anas <151745138+0xans@users.noreply.github.com>
2024-07-08 22:27:41 +0300

    Update listener.py

--- listener.py
+++ listener.py
@@ -9,14 +9,35 @@ def listener(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.bind((ip, port))
-        s.listen(5)
+        s.listen(1)
         print(f"{success} Listening on {ip}:{port}")
+        conn, addr = s.accept()
+        print(f"{success} Connection received from {addr[0]}:{addr[1]}")
         while True:
-            conn, addr = s.accept()
-            print(f"{success} Connection received from {addr[0]}:{addr[1]}")
-            handle(conn)
+            try:
+
+                ans = conn.recv(1024).decode()
+                sys.stdout.write(ans)
+                command = input(f"")
+                command += '\n'
+                if command.strip().lower() == 'exit':
+                    print(f"{info} Closing connection...")
+                    conn.close()
+                    break
+                conn.send(command.encode())
+                time.sleep(0.5)
+                _line = ans.split('\n')[-1]
+                sys.stdout.write('\033[A' + _line + '\n')
+            except KeyboardInterrupt:
+                print(f"{info} Interrupted by user. Closing connection...")
+                conn.close()
+                break
+            except Exception as e:
+                print(f"{error} An error occurred: {str(e)}")
+                conn.close()
+                break
     except KeyboardInterrupt:
-        return
+        print(f"{info} Interrupted by user. Exiting...")
     except socket.timeout:
         print(f"{error} Connection timed out.")
     except Exception as e:
@@ -24,33 +45,12 @@ def listener(ip, port):
     finally:
         s.close()
 
-def handle(conn):
-    try:
-        while True:
-            ans = conn.recv(1024).decode()
-            sys.stdout.write(ans)
-            command = input(f"{Fore.CYAN + Style.BRIGHT}")
-            command += f'{Fore.RESET + Style.RESET_ALL}\n'
-            if command.strip().lower() == 'exit':
-                print(f"{info} Closing connection...")
-                conn.close()
-                break
-            conn.send(command.encode())
-            time.sleep(0.5)
-            _line = ans.split('\n')[-1]
-            sys.stdout.write('\033[A' + _line + '\n')
-    except KeyboardInterrupt:
-        return
-    except Exception as e:
-        print(f"{error} An error occurred: {e}")
-    finally:
-        conn.close()
-        
 def main():
     parser = argparse.ArgumentParser(description="Listener Script")
     parser.add_argument("ip", nargs='?', default='0.0.0.0', help="Listener IP address")
     parser.add_argument("port", type=int, help="Listener port")
     args = parser.parse_args()
     listener(args.ip, args.port)
+
 if __name__ == '__main__':
     main()
